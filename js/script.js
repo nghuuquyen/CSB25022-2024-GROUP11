56e6cec89b2607298ef5448b297a4d57c33d0fb1
Gia Uy <74178707+uydeptrai@users.noreply.github.com>
2024-06-27 10:37:18 +0700

    update

--- js/script.js
+++ js/script.js
@@ -28,3 +28,18 @@ $(document).ready(function () {
         });
     });
 });
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+  dsn: "https://c74d51e0934e9eb35fbbb837d9a271f3@o4507501751369728.ingest.de.sentry.io/4507501758840912",
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});

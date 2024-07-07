c11d339bdf060c3b54feb2febeffdc7daa35777e
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2024-07-07 19:24:37 +0700

    Fix for deploy github page

--- js/script.js
+++ js/script.js
@@ -28,40 +28,40 @@ $(document).ready(function () {
         });
     });
 });
-// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
-const Sentry = require("@sentry/node");
-const { nodeProfilingIntegration } = require("@sentry/profiling-node");
-
-Sentry.init({
-  dsn: "https://c74d51e0934e9eb35fbbb837d9a271f3@o4507501751369728.ingest.de.sentry.io/4507501758840912",
-  integrations: [
-    nodeProfilingIntegration(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-});
-// IMPORTANT: Make sure to import `instrument.js` at the top of your file.
-// If you're using ECMAScript Modules (ESM) syntax, use `import "./instrument.js";`
-require("./instrument.js");
-
-// All other imports below
-const { createServer } = require("node:http");
-
-const server = createServer((req, res) => {
-  // server code
-});
-
-server.listen(3000, "127.0.0.1");
-Sentry.startSpan({
-    op: "test",
-    name: "My First Test Span",
-  }, () => {
-    try {
-      foo();
-    } catch (e) {
-      Sentry.captureException(e);
-    }
-  });
+// // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+// const Sentry = require("@sentry/node");
+// const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+//
+// Sentry.init({
+//   dsn: "https://c74d51e0934e9eb35fbbb837d9a271f3@o4507501751369728.ingest.de.sentry.io/4507501758840912",
+//   integrations: [
+//     nodeProfilingIntegration(),
+//   ],
+//   // Performance Monitoring
+//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
+//
+//   // Set sampling rate for profiling - this is relative to tracesSampleRate
+//   profilesSampleRate: 1.0,
+// });
+// // IMPORTANT: Make sure to import `instrument.js` at the top of your file.
+// // If you're using ECMAScript Modules (ESM) syntax, use `import "./instrument.js";`
+// require("./instrument.js");
+//
+// // All other imports below
+// const { createServer } = require("node:http");
+//
+// const server = createServer((req, res) => {
+//   // server code
+// });
+//
+// server.listen(3000, "127.0.0.1");
+// Sentry.startSpan({
+//     op: "test",
+//     name: "My First Test Span",
+//   }, () => {
+//     try {
+//       foo();
+//     } catch (e) {
+//       Sentry.captureException(e);
+//     }
+//   });
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: process.env.VITE_BASE_PATH || '/',
+    build: {
+        rollupOptions: {
+            input: {
+                'main': 'index.html',
+                'productpage': 'productpage.html',
+                'product-detail': 'product-detail.html',
+            }
+        }
+    }
+});
